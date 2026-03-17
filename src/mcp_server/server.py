2c080c74cfdf997fbbe1af52181409c09b4458f1
Patricio Del Boca <patriciodelboca@gmail.com>
2026-03-17 10:13:41 +0100

    Improve loading from python packages to include YAML files.

--- src/mcp_server/server.py
+++ src/mcp_server/server.py
@@ -2,25 +2,46 @@
 MCP Server with dynamic tool loading
 
 This server automatically discovers and loads tools from installed python packages.
-Each package should have an `ckan_mcp` entrypoint with a register_tools(mcp) function.
+Each package should have an `mcp_ckan` entrypoint with a register_tools(mcp) function.
 """
-import importlib.metadata
+import importlib
+import pkgutil
 
 from mcp.server.fastmcp import FastMCP
+
+from mcp_server.engines import load_dataset
 from mcp_server.settings import MCP_FETCH_REMOTE, MCP_TRANSPORT, MCP_HOST, MCP_PORT
 
 
-def load_plugins(mcp):
-    for entry_point in importlib.metadata.entry_points(group='ckan_mcp'):
-        print(f"Loading plugin: {entry_point.name}")
+def load_python_plugins(mcp):
+    """Load Python tools defined in plugins."""
+    for entry_point in importlib.metadata.entry_points(group='mcp_ckan'):
+        print(f"Loading plugin: {entry_point.module}")
         register_tools = entry_point.load()
         register_tools(mcp)
+
+
+def load_yaml_plugins(mcp):
+    """Load YAML tools defined in plugins.
+
+    The YAML tools are based on our engines. It will use a name-convention look for plugin
+    packages (like Flask).
+
+    https://packaging.python.org/en/latest/guides/creating-and-discovering-plugins/#using-naming-convention
+    """
+    discovered_plugins = [name for _, name, _ in pkgutil.iter_modules() if name.startswith('mcp_ckan_')]
+    for plugin in discovered_plugins:
+        resources = importlib.resources.files(plugin)
+        for resource in resources.rglob('*.yaml'):
+            print(f"Loading {resource}")
+            load_dataset(mcp, resource)
 
 
 def create_mcp_server(host, port):
     """Create MCP server with settings from environment variables"""
     mcp = FastMCP("Demo", host=host, port=port, streamable_http_path="/")
-    load_plugins(mcp)
+    load_python_plugins(mcp)
+    load_yaml_plugins(mcp)
     return mcp
 
 
