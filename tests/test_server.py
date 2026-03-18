8d55cd14684e0614eee2c79866e0d6925adb5055
Patricio Del Boca <patriciodelboca@gmail.com>
2026-03-18 09:09:04 +0100

    Add test for server.

---
+++ tests/test_server.py
@@ -0,0 +1,31 @@
+"""
+Initial MCP Server integration tests.
+
+Inspired on the official documentation: https://py.sdk.modelcontextprotocol.io/testing/
+
+"""
+from collections.abc import AsyncGenerator
+
+import pytest
+from mcp.client.session import ClientSession
+from mcp.shared.memory import create_connected_server_and_client_session
+from mcp.types import EmptyResult
+
+from mcp_server.server import mcp
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture
+async def client_session() -> AsyncGenerator[ClientSession]:
+    async with create_connected_server_and_client_session(mcp, raise_exceptions=True) as _session:
+        yield _session
+
+
+@pytest.mark.anyio
+async def test_server_runs(client_session: ClientSession):
+    result = await client_session.send_ping()
+    assert isinstance(result, EmptyResult)
